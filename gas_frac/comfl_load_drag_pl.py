6f2d5f199d71b9e7a57ad7cb2c76f48ac66020aa
Jason Furtney <jkfurtney@gmail.com>
2016-03-07 12:23:33 -0600

    adding notes on gas flow

---
+++ gas_frac/comfl_load_drag_pl.py
@@ -0,0 +1,142 @@
+"""
+left side input at high pressure?
+"""
+
+
+
+import numpy as np
+import pylab as pl
+
+def plot(x):
+    pl.plot(x)
+
+
+wl=np.logspace(-4,0,15)
+wl= wl.tolist()+[10]
+vlist=[]
+
+for w in wl:
+
+#inputs
+    N  = 2000
+    dl = 1.0
+    dt = 4e-7
+    endtime = 1.5e-3
+    RG = 287.0
+    gamma = 1.4
+
+    e_rough = 2.5e-2
+    varphi = 0.2*(e_rough/w)**0.5
+    drag   = varphi/2.0/w
+
+    p0l = 4e5
+    p0r = 1e5
+    p0c = 1e5
+    it0  = 300.0
+    iv0  = 0.0
+
+    x = np.linspace(0,dl,N)
+    r0,v0,t0 = np.zeros(N), np.zeros(N), np.zeros(N)
+
+    v0.fill(iv0)
+    t0.fill(it0)
+    p0 = np.zeros(N)
+    p0.fill(p0c)
+    p0[-1]=p0r
+    p0[0]=p0l
+
+    r0=p0/(RG*t0)
+
+
+    time=0
+    dx=dl/(N-1.0)
+
+
+    p=p0; t=t0; r=r0; v=v0
+
+    rho_r_0 = p0r/(RG*it0)
+    sp=np.sqrt(gamma*p0r/rho_r_0)*endtime
+
+
+    stop=int(endtime/dt)
+    i=0
+    v[-1]=v[-2]
+    r[-1]=r[-2]
+    t[-1]=t[-2]
+    p[-1]=p0r
+
+    v[0]=v[1]
+    r[0]=r[1]
+    t[0]=t[1]
+    p[0]=p0l
+
+    while i<stop*0.5:#<endtime:
+        i+=1
+        #calculate the derivatives.
+        # for each interior point
+        #calculate pressure from EoS Here.
+
+        # zero derivative and end condition
+
+        rl, rr, rc = np.roll(r,1)[1:-1], np.roll(r,-1)[1:-1],  r[1:-1]
+        vl, vr, vc = np.roll(v,1)[1:-1], np.roll(v,-1)[1:-1],  v[1:-1]
+        pll, pr    = np.roll(p,1)[1:-1], np.roll(p,-1)[1:-1]
+        tl, tr, tc = np.roll(t,1)[1:-1], np.roll(t,-1)[1:-1],  t[1:-1]
+
+        # initial derivatives
+        dr0 = -( rr*vr-rl*vl)/2.0/dx    
+        dv0 = -(pr-pll)/(2.0*rc*dx) - vc*((vc-vl)/dx) - drag*vc**2
+        dt0 = -(gamma-1)*tc*(vr-vl)/2.0/dx - vc*((tc-tl)/dx)
+
+        r1 = r.copy()
+        v1 = v.copy()
+        t1 = t.copy()
+
+        # prediction step
+        r1[1:-1] += dr0*dt 
+        v1[1:-1] += dv0*dt 
+        t1[1:-1] += dt0*dt     
+
+        # zero derivative and end condition
+        v1[-1]=v1[-2]
+        r1[-1]=r1[-2]
+        t1[-1]=t1[-2]
+        v1[0]=v1[1]
+        r1[0]=r1[1]
+        t1[0]=t1[1]
+        p1= r1*RG*t1
+        p1[-1]=p0r
+        p[0]=p0l
+
+        r1l, r1r, r1c = np.roll(r1,1)[1:-1], np.roll(r1,-1)[1:-1],  r1[1:-1]
+        v1l, v1r, v1c = np.roll(v1,1)[1:-1], np.roll(v1,-1)[1:-1],  v1[1:-1]
+        p1l, p1r      = np.roll(p1,1)[1:-1], np.roll(p1,-1)[1:-1]
+        t1l, t1r, t1c = np.roll(t1,1)[1:-1], np.roll(t1,-1)[1:-1],  t1[1:-1]    
+
+        # new derivatives
+        dr1 = -( r1r*v1r-r1l*v1l)/2.0/dx    
+        dv1 = -(p1r-p1l)/(2.0*r1c*dx) - v1c*((v1c-v1l)/dx) - drag*v1c**2
+        dt1 = -(gamma-1)*t1c*(v1r-v1l)/2.0/dx - v1c*((t1c-t1l)/dx)
+
+        # corrector step
+        r[1:-1] += 0.5*(dr0+dr1)*dt
+        v[1:-1] += 0.5*(dv0+dv1)*dt
+        t[1:-1] += 0.5*(dt0+dt1)*dt
+        v[-1]=v[-2]
+        r[-1]=r[-2]
+        t[-1]=t[-2]    
+        v[0]=v[1]
+        r[0]=r[1]
+        t[0]=t[1]    
+        p=r*RG*t
+        p[-1] = p0r
+        p[0]  = p0l
+
+
+    print w,v[0]
+    vlist.append(v[0])
+
+pl.semilogx(wl,vlist,linewidth=3)
+pl.xlabel('Fracture Aperture [m]')
+pl.ylabel('Fracture Gas Velocity [m/s]')
+pl.show()
